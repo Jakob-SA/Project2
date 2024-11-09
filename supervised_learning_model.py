988af8b97935988356ddaeaf97caa213ff85e85f
Jakob-SA <agergaardskovjakob@gmail.com>
2024-11-09 13:07:41 +0100

    Init

---
+++ supervised_learning_model.py
@@ -0,0 +1,227 @@
+import os
+import numpy as np
+import pandas as pd
+from dtuimldmtools import similarity
+import matplotlib.pyplot as plt
+from scipy.linalg import svd
+from sklearn.discriminant_analysis import StandardScaler
+from sklearn import model_selection
+from dtuimldmtools import rlr_validate
+
+from matplotlib.pylab import (
+    figure,
+    grid,
+    legend,
+    loglog,
+    semilogx,
+    show,
+    subplot,
+    title,
+    xlabel,
+    ylabel,
+)
+
+def summary_stats(data):
+    # Calculate summary statistics
+    means = data.mean()
+    stds = data.std()
+    medians = data.median()
+    ranges = data.max() - data.min()
+    quantiles = data.quantile([0.25, 0.5, 0.75])
+    # Create a summary DataFrame
+    summary_df = pd.DataFrame({
+        'Mean': means,
+        'Standard Deviation': stds,
+        'Median': medians,
+        'Range': ranges,
+        '25% Quantile': quantiles.loc[0.25],
+        '50% Quantile': quantiles.loc[0.5],
+        '75% Quantile': quantiles.loc[0.75]
+    })
+    return summary_df
+
+# Get the directory of the current script
+current_dir = os.path.dirname(__file__)
+
+# Construct the file path
+filename = os.path.join(current_dir, 'optical_interconnection_network_encoded.csv')
+# Load the optical_interconnection_network.csv data using the Pandas library
+df = pd.read_csv(filename, delimiter=',')
+
+df_subframe = df[(df['Node Number'] == 64) & (df['Thread Number'] == 4) ] 
+
+df_subframe = df_subframe.drop(['Node Number', 'Thread Number','Temporal Distribution_Asynchronous','Temporal Distribution_Client-Server' ], axis=1)
+
+print(df_subframe)
+
+
+
+scaler = StandardScaler()
+
+df_standardized = pd.DataFrame(scaler.fit_transform(df_subframe), columns = df_subframe.columns)
+
+
+
+
+summary = summary_stats(df_standardized)
+
+print(summary)
+
+
+
+X = df_standardized.drop(columns=['Channel Waiting Time'])
+y = df_standardized['Channel Waiting Time'].values
+
+print(X)
+print(y)
+
+# Add offset attribute (bias term)
+X = np.concatenate((np.ones((X.shape[0], 1)), X.values), axis=1)
+
+attributeNames = ["Offset"] + list(df.drop(columns=['Channel Waiting Time']).columns)
+N, M = X.shape  # Number of samples and features
+
+# Cross-validation setup
+K = 5
+CV = model_selection.KFold(K, shuffle=True)
+lambdas = np.power(10.0, range(-5, 9))
+
+Error_train = np.empty((K, 1))
+Error_test = np.empty((K, 1))
+Error_train_rlr = np.empty((K, 1))
+Error_test_rlr = np.empty((K, 1))
+Error_train_nofeatures = np.empty((K, 1))
+Error_test_nofeatures = np.empty((K, 1))
+w_rlr = np.empty((M, K))
+mu = np.empty((K, M - 1))
+sigma = np.empty((K, M - 1))
+w_noreg = np.empty((M, K))
+
+k = 0
+
+for train_index, test_index in CV.split(X, y):
+    # extract training and test set for current CV fold
+    X_train = X[train_index]
+    y_train = y[train_index]
+    X_test = X[test_index]
+    y_test = y[test_index]
+    internal_cross_validation = 10
+
+    (
+        opt_val_err,
+        opt_lambda,
+        mean_w_vs_lambda,
+        train_err_vs_lambda,
+        test_err_vs_lambda,
+    ) = rlr_validate(X_train, y_train, lambdas, internal_cross_validation)
+
+    # Standardize outer fold based on training set, and save the mean and standard
+    # deviations since they're part of the model (they would be needed for
+    # making new predictions) - for brevity we won't always store these in the scripts
+    mu[k, :] = np.mean(X_train[:, 1:], 0)
+    sigma[k, :] = np.std(X_train[:, 1:], 0)
+
+    X_train[:, 1:] = (X_train[:, 1:] - mu[k, :]) / sigma[k, :]
+    X_test[:, 1:] = (X_test[:, 1:] - mu[k, :]) / sigma[k, :]
+
+    Xty = X_train.T @ y_train
+    XtX = X_train.T @ X_train
+
+    # Compute mean squared error without using the input data at all
+    Error_train_nofeatures[k] = (
+        np.square(y_train - y_train.mean()).sum(axis=0) / y_train.shape[0]
+    )
+    Error_test_nofeatures[k] = (
+        np.square(y_test - y_test.mean()).sum(axis=0) / y_test.shape[0]
+    )
+
+    # Estimate weights for the optimal value of lambda, on entire training set
+    lambdaI = opt_lambda * np.eye(M)
+    lambdaI[0, 0] = 0  # Do no regularize the bias term
+    w_rlr[:, k] = np.linalg.solve(XtX + lambdaI, Xty).squeeze()
+    # Compute mean squared error with regularization with optimal lambda
+    Error_train_rlr[k] = (
+        np.square(y_train - X_train @ w_rlr[:, k]).sum(axis=0) / y_train.shape[0]
+    )
+    Error_test_rlr[k] = (
+        np.square(y_test - X_test @ w_rlr[:, k]).sum(axis=0) / y_test.shape[0]
+    )
+
+    # Estimate weights for unregularized linear regression, on entire training set
+    w_noreg[:, k] = np.linalg.solve(XtX, Xty).squeeze()
+    # Compute mean squared error without regularization
+    Error_train[k] = (
+        np.square(y_train - X_train @ w_noreg[:, k]).sum(axis=0) / y_train.shape[0]
+    )
+    Error_test[k] = (
+        np.square(y_test - X_test @ w_noreg[:, k]).sum(axis=0) / y_test.shape[0]
+    )
+    # OR ALTERNATIVELY: you can use sklearn.linear_model module for linear regression:
+    # m = lm.LinearRegression().fit(X_train, y_train)
+    # Error_train[k] = np.square(y_train-m.predict(X_train)).sum()/y_train.shape[0]
+    # Error_test[k] = np.square(y_test-m.predict(X_test)).sum()/y_test.shape[0]
+
+    # Display the results for the last cross-validation fold
+    if k == K - 1:
+        figure(k, figsize=(12, 8))
+        subplot(1, 2, 1)
+        semilogx(lambdas, mean_w_vs_lambda.T[:, 1:], ".-")  # Don't plot the bias term
+        xlabel("Regularization factor")
+        ylabel("Mean Coefficient Values")
+        grid()
+        # You can choose to display the legend, but it's omitted for a cleaner
+        # plot, since there are many attributes
+        # legend(attributeNames[1:], loc='best')
+
+        subplot(1, 2, 2)
+        title("Optimal lambda: 1e{0}".format(np.log10(opt_lambda)))
+        loglog(
+            lambdas, train_err_vs_lambda.T, "b.-", lambdas, test_err_vs_lambda.T, "r.-"
+        )
+        xlabel("Regularization factor")
+        ylabel("Squared error (crossvalidation)")
+        legend(["Train error", "Validation error"])
+        grid()
+
+    # To inspect the used indices, use these print statements
+    # print('Cross validation fold {0}/{1}:'.format(k+1,K))
+    # print('Train indices: {0}'.format(train_index))
+    # print('Test indices: {0}\n'.format(test_index))
+
+    k += 1
+
+show()
+# Display results
+print("Linear regression without feature selection:")
+print("- Training error: {0}".format(Error_train.mean()))
+print("- Test error:     {0}".format(Error_test.mean()))
+print(
+    "- R^2 train:     {0}".format(
+        (Error_train_nofeatures.sum() - Error_train.sum())
+        / Error_train_nofeatures.sum()
+    )
+)
+print(
+    "- R^2 test:     {0}\n".format(
+        (Error_test_nofeatures.sum() - Error_test.sum()) / Error_test_nofeatures.sum()
+    )
+)
+print("Regularized linear regression:")
+print("- Training error: {0}".format(Error_train_rlr.mean()))
+print("- Test error:     {0}".format(Error_test_rlr.mean()))
+print(
+    "- R^2 train:     {0}".format(
+        (Error_train_nofeatures.sum() - Error_train_rlr.sum())
+        / Error_train_nofeatures.sum()
+    )
+)
+print(
+    "- R^2 test:     {0}\n".format(
+        (Error_test_nofeatures.sum() - Error_test_rlr.sum())
+        / Error_test_nofeatures.sum()
+    )
+)
+
+print("Weights in last fold:")
+for m in range(M):
+    print("{:>15} {:>15}".format(attributeNames[m], np.round(w_rlr[m, -1], 2)))
